d7639916559ee5735d9ef5446510338f44661e6e
OwlRepo <romeoangeles015017@gmail.com>
2023-03-01 16:48:09 +0800

    feat: initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#eb2426',
+          secondary: '#33eab0',
+          accent: '#74f7a8',
+          neutral: '#1C2226',
+          'base-100': '#F4F4F6',
+          info: '#85A5D6',
+          success: '#1EB395',
+          warning: '#F99634',
+          error: '#EB4D2D',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+}
+
+module.exports = config
